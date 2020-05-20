e985c00bb6060b3f2ac2ac711433d0ba658dad52
snstrauss <zahavi.guy.85@gmail.com>
2020-05-20 15:09:38 +0300

    Updates

---
+++ precache-manifest.6cd0675a0db1053c76e17d4e42475db3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8d79214a53305b697c9e64f05b8513",
+    "url": "/snstrauss.github.io/youtube-mock/index.html"
+  },
+  {
+    "revision": "412efbf52686ccfd0152",
+    "url": "/snstrauss.github.io/youtube-mock/static/css/main.80771197.chunk.css"
+  },
+  {
+    "revision": "ceada941547c7ad13010",
+    "url": "/snstrauss.github.io/youtube-mock/static/js/2.f68b0174.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snstrauss.github.io/youtube-mock/static/js/2.f68b0174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "412efbf52686ccfd0152",
+    "url": "/snstrauss.github.io/youtube-mock/static/js/main.51edb90a.chunk.js"
+  },
+  {
+    "revision": "9b6b4c2a9e27413715fd",
+    "url": "/snstrauss.github.io/youtube-mock/static/js/runtime-main.0314d287.js"
+  }
+]);

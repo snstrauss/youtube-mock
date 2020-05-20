373681a6b6580cf2819a29b6f2a24d49037592ee
snstrauss <zahavi.guy.85@gmail.com>
2020-05-20 15:21:11 +0300

    Updates

---
+++ precache-manifest.8fc37a8d95c82dda338458bba04944de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2819c3cdf43ac8c2ad234ff49250aa6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b93417cf7f22622bfc36",
+    "url": "/static/css/main.80771197.chunk.css"
+  },
+  {
+    "revision": "ceada941547c7ad13010",
+    "url": "/static/js/2.f68b0174.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f68b0174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b93417cf7f22622bfc36",
+    "url": "/static/js/main.e90b3772.chunk.js"
+  },
+  {
+    "revision": "9c6bbe695edb9170b4e3",
+    "url": "/static/js/runtime-main.278f1f8a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/snstrauss.github.io/youtube-mock/precache-manifest.3010532c5b7d533731aee24876d14ed4.js"
+  "/precache-manifest.8fc37a8d95c82dda338458bba04944de.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/snstrauss.github.io/youtube-mock/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

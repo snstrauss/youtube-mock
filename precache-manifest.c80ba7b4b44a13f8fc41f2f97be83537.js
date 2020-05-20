b8ce73b64e1c3378f85a35c8a243c2982421e81e
snstrauss <zahavi.guy.85@gmail.com>
2020-05-20 15:33:14 +0300

    Updates

--- precache-manifest.c80ba7b4b44a13f8fc41f2f97be83537.js
+++ precache-manifest.c80ba7b4b44a13f8fc41f2f97be83537.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2819c3cdf43ac8c2ad234ff49250aa6a",
+    "revision": "21c750369829751b96e0d9e681e7f7a3",
     "url": "/index.html"
   },
   {
-    "revision": "b93417cf7f22622bfc36",
-    "url": "/static/css/main.80771197.chunk.css"
+    "revision": "15fd3bcfab8fbacf302c",
+    "url": "/static/css/main.a76380f7.chunk.css"
   },
   {
     "revision": "ceada941547c7ad13010",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.f68b0174.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b93417cf7f22622bfc36",
+    "revision": "15fd3bcfab8fbacf302c",
     "url": "/static/js/main.e90b3772.chunk.js"
   },
   {
